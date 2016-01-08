f3e87eeb2e0795a5642f47cd465d0fe1fd15f5d7
ohager <oliver.hager@dextra.com.br>
2016-01-07 22:27:04 -0200

    Multiple Action Indicators on FluxSimulator (using snabbt for animation)

---
+++ js/views/components/fluxsimulator/click.indicator.js
@@ -0,0 +1,67 @@
+define(function (require) {
+
+    var React = require('react');
+    var Snabbt = require('snabbt');
+
+    return React.createClass({
+
+        propTypes : {
+            name : React.PropTypes.string.isRequired,
+            position : React.PropTypes.shape({
+                x: React.PropTypes.number.isRequired,
+                y: React.PropTypes.number.isRequired
+            }).isRequired,
+            onAnimationEnd : React.PropTypes.func
+        },
+
+        getInitialState: function () {
+            return {show: true}
+        },
+
+        componentWillMount: function () {
+        },
+
+        componentDidMount: function () {
+
+            if(!this.refs.indicator) return;
+
+            var indicator = this.refs.indicator.getDOMNode();
+            Snabbt(indicator, {
+                scale : [2.0,2.0],
+                opacity: 0.0,
+                fromOpacity: 1.0,
+                duration: 750,
+                easing: 'ease',
+                allDone: function(){
+                    this.setState({show: false});
+                    if(this.props.onAnimationEnd) {
+                        this.props.onAnimationEnd();
+                    }
+                }.bind(this)
+            })
+        },
+
+        componentWillUnmount: function () {
+
+            if(!this.refs.indicator) return;
+
+        },
+        render: function () {
+
+            var divStyle = {
+                top: this.props.position.y - 30 + 'px',
+                left: this.props.position.x - 30 + 'px'
+            };
+
+            var classes = "click-indicator";
+
+            return (
+                    this.state.show ?
+                    React.createElement("div", {ref: "indicator", className: "click-indicator", style: divStyle}, 
+                        React.createElement("small", null, this.props.name)
+                    )
+                        :null
+            )
+        }
+    })
+});
---
+++ js/views/components/fluxsimulator/visualization.container.js
@@ -0,0 +1,52 @@
+define(function (require) {
+
+    require('component/fluxsimulator/fluxconfig');
+    var React = require('react');
+    var NanoFlux = require('nanoflux');
+    var ClickIndicator = require('component/fluxsimulator/click.indicator');
+
+    return React.createClass({
+
+        visualizationStore: NanoFlux.getStore('visualizationStore'),
+        userActions: NanoFlux.getActions('visualizationActions'),
+        _subscription: null,
+
+        getInitialState: function () {
+            return { actions : [] }
+        },
+
+        onActionTriggered: function (action) {
+            if(this.isMounted()) {
+                var actions = this.state.actions;
+                actions.push(action);
+                this.setState({actions : actions});
+            }
+        },
+
+        componentWillMount: function () {
+            this.visualizationStore.subscribe(this, this.onActionTriggered);
+        },
+
+        componentDidMount: function () {
+        },
+
+        componentWillUnmount: function () {
+        },
+
+        removeIndicator : function(){
+            //this.setState( {action : null} );
+        },
+
+        render: function () {
+
+            return (
+                React.createElement("div", null, 
+                    this.state.actions.map(function(action, index){
+                            return React.createElement(ClickIndicator, {key: index, name: action.name, position: action.pos})
+                        })
+                    
+                )
+            )
+        }
+    })
+});
